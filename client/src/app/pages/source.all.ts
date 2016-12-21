f791081d5b1cc8892cae9e0464be617a47f3aefd
zlm <zlm>
2016-12-21 21:50:57 +0800

    21

--- client/src/app/pages/source.all.ts
+++ client/src/app/pages/source.all.ts
@@ -1,57 +1,66 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { AppState, Global } from '../shared/services/app.service';
+// system import or require
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'
+import { AppState, Global } from '../shared/services/app.service'
+var ec = require('echarts')
 
-declare var $: any;
-var ec = require('echarts');
+// private import
+import { SourceService } from '../shared/services/source.service'
+
+declare var $: any
 
 @Component({
   selector: 'source',
   templateUrl: './source.all.html',
-  styleUrls: ['source.all.scss']
+  styleUrls: ['source.all.scss'],
+  providers: [
+    SourceService
+  ],
 })
 export class SourceAllPage implements OnInit {
+
   // 时间范围(通过updateDaterangeI函数自动获取到页面所选内容)
   selectedDaterange = {
     startDate: '2016-01-01',
     endDate: '2016-03-01'
-  };
-  // 埋点网站(页面顶部下拉框所选内容)，因为self.selectedSiteID.unshift(value);只能定义成数组类型
-  selectedSiteID = [];
-  // 分析类型：来源类型/来源网站
-  anaType: number;
+  }
 
-  constructor(public _appState: AppState, private _global: Global, private ref: ChangeDetectorRef) {
-    var self = this;
+  // 埋点网站(页面顶部下拉框所选内容)，因为self.selectedSiteID.unshift(value);只能定义成数组类型
+  selectedSiteID = []
+
+  // 分析类型：来源类型/来源网站
+  anaType: number
+
+  constructor(
+    public sourceService: SourceService,
+    public appState: AppState,
+    private _global: Global,
+    private _ref: ChangeDetectorRef) {
+
+    // self this
+    var self = this
 
     /**
      * 订阅分析网站变更事件
      * 取值用this.selectedSiteID[0]获取
      */
-    _appState.siteSelectedChange.subscribe((value: any) => {
-      self.selectedSiteID.unshift(value);
-      self.ref.markForCheck();
-      self.ref.detectChanges();
-      self.selectedSiteID[0] = value;
+    appState.siteSelectedChange.subscribe((value: any) => {
+      self.selectedSiteID.unshift(value)
+      self._ref.markForCheck()
+      self._ref.detectChanges()
+      self.selectedSiteID[0] = value
 
       // render page
-      this.renderPage();
+      this.renderPage()
     })
   }
 
   ngOnInit() {
-    this._global.init();
-    this.renderPage();
+    this._global.init()
+    this.renderPage()
   }
 
-  private getSearchCondition() {
-
-  }
-
-  setPie_source_type() {
-
-    
-
-    var myChart = ec.init(document.getElementById('pie_source_type'));
+  setEchartPie_sourceType_today_pv(data) {
+    var myChart = ec.init(document.getElementById('pie_source_type'))
     var option = {
       tooltip: {
         trigger: 'item',
@@ -59,9 +68,8 @@ export class SourceAllPage implements OnInit {
       },
       legend: {
         y: 'bottom',
-        data: ['搜索引擎', '外部访问', '直接访问']
+        data: ['搜索引擎', '外部链接', '直接访问']
       },
-
       toolbox: {
         show: true,
         feature: {
@@ -70,15 +78,14 @@ export class SourceAllPage implements OnInit {
           restore: { show: true },
           saveAsImage: { show: true }
         }
-      },      
-
+      },
       calculable: true,
       series: [
         {
           name: '来源类型',
           type: 'pie',
-          radius : ['30%', '60%'],
-          itemStyle: 　{
+          radius: ['30%', '60%'],
+          itemStyle: {
             normal: {
               label: {
                 show: false
@@ -98,70 +105,61 @@ export class SourceAllPage implements OnInit {
               }
             }
           },
-          data: [
-            { value: 335, name: '搜索引擎' },
-            { value: 310, name: '外部访问' },
-            { value: 234, name: '直接访问' }
-          ]
+          // data: [
+          //   { value: 335, name: '搜索引擎' },
+          //   { value: 310, name: '外部访问' },
+          //   { value: 234, name: '直接访问' }
+          // ]
+          data: data
         }
       ]
     };
     myChart.setOption(option);
   }
 
-  setLine_source_type() {
+  /**
+   * 来源类型折线图
+   * 范围：今日
+   * 分析指标：浏览量（PV）
+   */
+  setEchartLine_sourceType_today_pv(data) {
 
-    var title = {
-      text: "ECharts简单线形图表及其配置展示实例", //正标题  
-      link: "http://www.xxx.com", //正标题链接 点击可在新窗口中打开  
-      x: "center", //标题水平方向位置  
-      subtext: "From:http://www.xxx.com", //副标题  
-      sublink: "http://www.xxx.com", //副标题链接  
-      //正标题样式  
-      textStyle: {
-        fontSize: 24
-      },
-      //副标题样式  
-      subtextStyle: {
-        fontSize: 12,
-        color: "red"
-      }
-    };
+    var day_xAxisData = ['00:00~00:59', '01:00~01:59', '02:00~02:59', '03:00~03:59', '04:00~04:59', 
+      '05:00~05:59', '06:00~06:59', '07:00~07:59', '08:00~08:59', '09:00~09:59', '10:00~10:59', '11:00~11:59', '12:00~12:59', 
+      '13:00~13:59', '14:00~14:59', '15:00~15:59', '16:00~16:59', '17:00~17:59', '18:00~18:59', '19:00~19:59', '20:00~20:59', 
+      '21:00~21:59', '22:00~22:59', '23:00~23:59']
 
     var option = {
-      //图表标题  
-      // title: title,
-
-      //数据提示框配置  
       tooltip: {
-        trigger: 'axis' //触发类型，默认数据触发，见下图，可选为：'item' | 'axis' 其实就是是否共享提示框  
+        extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);',
+        trigger: 'axis'
       },
-      //图例配置  
       legend: {
-        data: ['搜索引擎', '外部链接', '直接访问'], //这里需要与series内的每一组数据的name值保持一致  
+        data: ['搜索引擎', '外部链接', '直接访问'],
         y: "bottom"
       },
-      //工具箱配置  
       toolbox: {
         show: true,
         feature: {
-          mark: { show: true }, // 辅助线标志，上图icon左数1/2/3，分别是启用，删除上一条，删除全部  
-          dataView: { show: true, readOnly: false },// 数据视图，上图icon左数8，打开数据视图  
-          magicType: { show: true, type: ['line', 'bar', 'stack', 'tiled'] },// 图表类型切换，当前仅支持直角系下的折线图、柱状图转换，上图icon左数6/7，分别是切换折线图，切换柱形图  
-          restore: { show: true }, // 还原，复位原始图表，上图icon左数9，还原  
-          saveAsImage: { show: true } // 保存为图片，上图icon左数10，保存  
+          mark: { show: true },
+          dataView: { show: true, readOnly: false },
+          magicType: { show: true, type: ['line', 'bar', 'stack', 'tiled'] },
+          restore: { show: true },
+          saveAsImage: { show: true }
         }
       },
       calculable: true,
-      //轴配置  
       xAxis: [
         {
           type: 'category',
-          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-          name: "月份"
+          name: "时间",
+          boundaryGap : false,
+          data: day_xAxisData,
+          axisLabel: {
+            interval: 3
+          }
         }
       ],
-      //Y轴配置  
       yAxis: [
         {
           type: 'value',
@@ -169,177 +167,128 @@ export class SourceAllPage implements OnInit {
           name: "数值"
         }
       ],
-      //图表Series数据序列配置  
       series: [
         {
           name: '搜索引擎',
           type: 'line',
-          data: [1.6, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+          data: [1.6, 4.9, 7.0, 23.2, 25.6, 29,1.6, 4.9, 7.0, 23.2, 25.6, 29, 1.6, 4.9, 7.0, 23.2, 25.6, 29,1.6, 4.9, 7.0, 23.2, 25.6, 29]
         },
         {
           name: '外部链接',
           type: 'line',
-          data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+          data: [2.6, 5.9, 9.0, 26.4, 28.7, 30, 2.6, 5.9, 9.0, 26.4, 28.7, 30, 2.6, 5.9, 9.0, 26.4, 28.7, 30, 2.6, 5.9, 9.0, 26.4, 28.7, 30]
         },
         {
           name: '直接访问',
           type: 'line',
-          data: [3.6, 6.9, 11.0, 28.4, 30.7, 72.7, 177.6, 185.2, 50.7, 20.8, 8.0, 4.3]
+          data: [3.6, 6.9, 11.0, 28.4, 30.7, 31, 3.6, 6.9, 11.0, 28.4, 30.7, 31, 3.6, 6.9, 11.0, 28.4, 30.7, 31, 3.6, 6.9, 11.0, 28.4, 30.7, 31]
         }
       ]
-    };
-    
-    
-    
-    //图表渲染的容器对象  
-    var chartContainer = document.getElementById("line_source_type");  
+    }
+
     //加载图表  
-    var myChart = ec.init(chartContainer);  
-    myChart.setOption(option);  
+    var myChart = ec.init(document.getElementById("line_source_type"))
+    myChart.setOption(option)
   }
 
   ngAfterContentInit() { }
 
-
-
-  test() {
-    $("#mytree").jqGrid({   
-      treeGrid: true,  
-      treeGridModel: 'adjacency', //treeGrid模式，跟json元数据有关  
-      ExpandColumn : 'username',       
-      scroll: "true",  
-      "url":"assets/data/data.json",
-      datatype: 'json',      
-      colNames:['编号','姓名','密码','年龄','地址','出生日期'],      
-      colModel:[      
-          {name:'id',index:'id', width:90,sorttype:"int"},      
-          {name:'username',index:'username', width:110,sorttype:"int"},      
-          {name:'password',index:'password', width:80},      
-          {name:'age',index:'age', width:80},        
-          {name:'address',index:'address', width:80},       
-          {name:'time',index:'time', width:80,sorttype:"date"}        
-       ],  
-      pager: "false",    
-      sortname: 'id',      
-      sortorder: "desc",   
-          
-      jsonReader: {      
-        root: "dataRows",    
-        repeatitems : false      
-      },      
-      treeReader : {  
-        level_field: "level",  
-        parent_id_field: "parent",   
-        leaf_field: "isLeaf",  
-        expanded_field: "expanded"  
-      },  
-      caption: "jqGrid test",     
-      mtype: "POST",  
-      height: "auto",    // 设为具体数值则会根据实际记录数出现垂直滚动条  
-      rowNum : "-1",     // 显示全部记录  
-      shrinkToFit:false  // 控制水平滚动条  
-   });       
-  }
-
+  /**
+   * treegrid setup
+   */
   setupTreeData() {
     $('#mytree').jqGrid({
-				"url":"assets/data/data.json",
-				"colModel":[
-					{
-						"name":"id",
-						"index":"id",
-						"sorttype":"int",
-						"key":true,
-						"hidden":true,
-						"width":50
-					},{
-						"name":"name",
-						"index":"name",
-						"sorttype":"string",
-						"label":"来源类型",
-						"width":120
-					},{
-						"name":"pv",
-						"index":"pv",
-						"sorttype":"int",
-						"label":"浏览量",
-						// "width":90,
-						"align":"right"
-					},{
-						"name":"pv_ratio",
-						"index":"pv_ratio",
-						"sorttype":"number",
-						"label":"浏览量占比(%)",
-						// "width":90,
-						"align":"right"
-					},{
-						"name":"visit_count",
-						"index":"visit_count",
-						"sorttype":"string",
-						"label":"访问次数",
-						// "width":100
-            "align":"right"
-					},{
-						"name":"uv",
-						"index":"uv",
-						"sorttype":"int",
-						"label":"访客数",
-						// "width":100
-            "align":"right"
-					},{
-						"name":"visit_average_time",
-						"index":"visit_average_time",
-						"sorttype":"string",
-						"label":"平均访问时长",
-						// "width":100
-            "align":"right"
-					},{
-						"name":"lft",
-						"hidden":true
-					},{
-						"name":"rgt",
-						"hidden":true
-					},{
-						"name":"level",
-						"hidden":true
-					},{
-						"name":"uiicon",
-						"hidden":true
-					}
-				],
-				"width":$('.tab-content').width()-15,
-				"hoverrows":false,
-				"viewrecords":false,
-				"gridview":true,
-				"height":"auto",
-				"sortname":"lft",
-				"loadonce":true,
-				"rowNum":100,
-				"scrollrows":true,
-				// enable tree grid
-				"treeGrid":true,
-				// which column is expandable
-				"ExpandColumn":"name",
-				// expand a node when click on the node name 
-				"ExpandColClick" : true,
-				// datatype
-				"treedatatype":"json",
-				// the model used
-				"treeGridModel":"nested",
-				// configuration of the data comming from server
-				"treeReader":{
-					"left_field":"lft",
-					"right_field":"rgt",
-					"level_field":"level",
-					"leaf_field":"isLeaf",
-					"expanded_field":"expanded",
-					"loaded":"loaded",
-					"icon_field":"icon"
-				},
-				"sortorder":"asc",
-				"datatype":"json",
-				"pager":"#pager"
-			}); 
+      "url": "assets/data/data.json",
+      "colModel": [
+        {
+          "name": "id",
+          "index": "id",
+          "sorttype": "int",
+          "key": true,
+          "hidden": true,
+          "width": 50
+        }, {
+          "name": "name",
+          "index": "name",
+          "sorttype": "string",
+          "label": "来源类型",
+          "width": 120
+        }, {
+          "name": "pv",
+          "index": "pv",
+          "sorttype": "int",
+          "label": "浏览量",
+          // "width":90,
+          "align": "right"
+        }, {
+          "name": "pv_ratio",
+          "index": "pv_ratio",
+          "sorttype": "number",
+          "label": "浏览量占比(%)",
+          // "width":90,
+          "align": "right"
+        }, {
+          "name": "visit_count",
+          "index": "visit_count",
+          "sorttype": "string",
+          "label": "访问次数",
+          // "width":100
+          "align": "right"
+        }, {
+          "name": "uv",
+          "index": "uv",
+          "sorttype": "int",
+          "label": "访客数",
+          // "width":100
+          "align": "right"
+        }, {
+          "name": "visit_average_time",
+          "index": "visit_average_time",
+          "sorttype": "string",
+          "label": "平均访问时长",
+          // "width":100
+          "align": "right"
+        }, {
+          "name": "lft",
+          "hidden": true
+        }, {
+          "name": "rgt",
+          "hidden": true
+        }, {
+          "name": "level",
+          "hidden": true
+        }, {
+          "name": "uiicon",
+          "hidden": true
+        }
+      ],
+      "width": $('.tab-content').width() - 15,
+      "hoverrows": false,
+      "viewrecords": false,
+      "gridview": true,
+      "height": "auto",
+      "sortname": "lft",
+      "loadonce": true,
+      "rowNum": 100,
+      "scrollrows": true,
+      "treeGrid": true,
+      "ExpandColumn": "name",
+      "ExpandColClick": true,
+      "treedatatype": "json",
+      "treeGridModel": "nested",
+      "treeReader": {
+        "left_field": "lft",
+        "right_field": "rgt",
+        "level_field": "level",
+        "leaf_field": "isLeaf",
+        "expanded_field": "expanded",
+        "loaded": "loaded",
+        "icon_field": "icon"
+      },
+      "sortorder": "asc",
+      "datatype": "json",
+      "pager": "#pager"
+    })
   }
 
   /**
@@ -355,8 +304,8 @@ export class SourceAllPage implements OnInit {
    * @memberOf SourceAllPage
    */
   updateDaterangeI(obj) {
-    this.selectedDaterange = obj;
-    this.renderPage();
+    this.selectedDaterange = obj
+    this.renderPage()
   }
 
 
@@ -364,28 +313,32 @@ export class SourceAllPage implements OnInit {
    * 页面展现
    */
   renderPage() {
+    var self = this
 
-    console.log('>>> begin render page');
-    console.log('目标网站ID：' + this.selectedSiteID[0]);
-    console.log('日期范围：' + this.selectedDaterange.startDate + ' ~ ' + this.selectedDaterange.endDate);
+    console.log('>>> begin render page')
+    console.log('目标网站ID：' + this.selectedSiteID[0])
+    console.log('日期范围：' + this.selectedDaterange.startDate + ' ~ ' + this.selectedDaterange.endDate)
 
-    var targetSite = this.selectedSiteID[0];
-    var startDate = this.selectedDaterange.startDate;
-    var endDate = this.selectedDaterange.endDate;
-    
-    if ( ! targetSite ) {
-      //alert('您未选择要分析的目标网站');
-      return ;
+    var siteId = this.selectedSiteID[0]
+    var startDate = this.selectedDaterange.startDate
+    var endDate = this.selectedDaterange.endDate
+
+    if (!siteId) {
+      //alert('您未选择要分析的目标网站')
+      return
     }
 
-    // 树结构数据
-    this.setupTreeData();
+    //todo: test data
+    startDate = '2010-03-02'
+    endDate = '2016-12-12'
+    siteId = 1
 
-    // echarts组件
-    this.setPie_source_type();
-    this.setLine_source_type();    
-
+    self.sourceService.getDataWithType(siteId, startDate, endDate)
+      .then(data => {
+        console.log(data)
+        self.setEchartPie_sourceType_today_pv(data.pieData)
+        self.setEchartLine_sourceType_today_pv(data.lineData)
+        self.setupTreeData()
+      }).catch(err => { })
   }
-  
-
 }
